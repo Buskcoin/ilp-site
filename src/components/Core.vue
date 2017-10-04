aa4c406e0fbe968c32d41e2b481df5630bafe5f4
Keith <keith.groves@jointheleague.org>
2017-10-04 07:38:29 -0700

    update core

--- src/components/Core.vue
+++ src/components/Core.vue
@@ -1,28 +1,26 @@
 <template>
   <div class="core">
-<!--    <h1>{{ msg }}</h1>
-    <h2>Goodbye {{ $route.params.name}}</h2>
-    -->
-     <div id="wsite-content" class="wsite-elements wsite-not-footer">
-	<h2 class="wsite-content-title" style="text-align:left;">Core Outcome 1: Professional Communication &amp; Development</h2>
 
-<div class="paragraph" style="text-align:left;"><strong>Plan and assess personal career options and develop communication skills.</strong><ul><li>Acquire, analyze and use information about the range of career possibilities, industry trends in computer science and information technology and its related fields to make career planning decisions.</li><li>Use critical thinking skills to plan and assess personal career and further study goals and design educational experiences directed to those goals.</li><li>Use written and oral communication to deliver persuasive viewpoint and technology content to multiple audiences.</li><li>Acquire, analyze and use information about computer sciences and information technology to describe and speculate on the impact of digital communications technology on local and global communities.</li></ul><br />&#8203;<strong>Satisfied With:</strong><ul><li><strong><a href="/cst-300-major-proseminar.html">CST 300, Major Proseminar</a></strong></li></ul></div>
+     <div id="csumb-content" >
+	<h2>Core Outcome 1: Professional Communication &amp; Development</h2>
 
-<h2 class="wsite-content-title" style="text-align:left;">Core Outcome 2: Knowledge of Computers &amp; Networks</h2>
+<div class="paragraph">Plan and assess personal career options and develop communication skills.<ul><li>Acquire, analyze and use information about the range of career possibilities, industry trends in computer science and information technology and its related fields to make career planning decisions.</li><li>Use critical thinking skills to plan and assess personal career and further study goals and design educational experiences directed to those goals.</li><li>Use written and oral communication to deliver persuasive viewpoint and technology content to multiple audiences.</li><li>Acquire, analyze and use information about computer sciences and information technology to describe and speculate on the impact of digital communications technology on local and global communities.</li></ul><br />&#8203;Satisfied With:</strong><ul><li><a href="/cst-300-major-proseminar.html">CST 300, Major Proseminar</a></li></ul></div>
 
-<div class="paragraph" style="text-align:left;"><strong>Demonstrate an understanding of the fundamentals of computer architecture, operating systems and networks.</strong><ul><li>Describe the history of computers and computing.</li><li>Explain how a computer represents and transmits information, how a computer's parts and components are organized and interfaced, basic components of an operating system and basic components of local area networks and wide area networks.</li><li>Analyze and design simple circuits using digital electronics and logic gates.</li><li>Describe the structure and organization of a modern microprocessor, and the sequence of operations in a typical instruction execution.</li><li>Demonstrate a basic understanding of Assembly Language programming.</li></ul><strong>Satisfied With:</strong><ul><li>&#8203;<strong><a href="/cst-311-intro-to-computer-networks.html">&#8203;CST 311, Intro to Computer Networks</a></strong></li></ul></div>
+<h2 >Core Outcome 2: Knowledge of Computers &amp; Networks</h2>
 
-<h2 class="wsite-content-title" style="text-align:left;">Core Outcome 3: Programming &amp; Problem Solving</h2>
+<div class="paragraph"><strong>Demonstrate an understanding of the fundamentals of computer architecture, operating systems and networks.</strong><ul><li>Describe the history of computers and computing.</li><li>Explain how a computer represents and transmits information, how a computer's parts and components are organized and interfaced, basic components of an operating system and basic components of local area networks and wide area networks.</li><li>Analyze and design simple circuits using digital electronics and logic gates.</li><li>Describe the structure and organization of a modern microprocessor, and the sequence of operations in a typical instruction execution.</li><li>Demonstrate a basic understanding of Assembly Language programming.</li></ul><strong>Satisfied With:</strong><ul><li>&#8203;<strong><a href="/cst-311-intro-to-computer-networks.html">&#8203;CST 311, Intro to Computer Networks</a></strong></li></ul></div>
 
-<div class="paragraph" style="text-align:left;"><strong>Analyze and solve problems using a programming language.</strong><ul><li>Demonstrate the ability to develop algorithms and computer programs to solve problems by systematically decomposing a complex programming problem into manageable, well-documented steps that results in a design for the problem solution.</li><li>Demonstrate that the program compiles and executes correctly for a given set of data.</li><li>Demonstrate programming competency in a variety of programming structures, including arithmetic operations, control structures and loops, input/output operations including file I/O, the use of function calls, and the use of arrays and other storage types.</li><li>Debug a program using appropriate debugging strategies.</li><li>Produce a complete set of documentation for a program.</li></ul>&#8203;<strong>Satisfied With:</strong><ul><li><strong>CST 231, Problem-Solving/Programming</strong></li><li><strong>CST 238, Intro to Data Structures</strong></li></ul></div>
+<h2  >Core Outcome 3: Programming &amp; Problem Solving</h2>
 
-<h2 class="wsite-content-title" style="text-align:left;">Core Outcome 4: Mathematics for Computing</h2>
+<div class="paragraph" ><strong>Analyze and solve problems using a programming language.</strong><ul><li>Demonstrate the ability to develop algorithms and computer programs to solve problems by systematically decomposing a complex programming problem into manageable, well-documented steps that results in a design for the problem solution.</li><li>Demonstrate that the program compiles and executes correctly for a given set of data.</li><li>Demonstrate programming competency in a variety of programming structures, including arithmetic operations, control structures and loops, input/output operations including file I/O, the use of function calls, and the use of arrays and other storage types.</li><li>Debug a program using appropriate debugging strategies.</li><li>Produce a complete set of documentation for a program.</li></ul>&#8203;<strong>Satisfied With:</strong><ul><li><strong>CST 231, Problem-Solving/Programming</strong></li><li><strong>CST 238, Intro to Data Structures</strong></li></ul></div>
 
-<div class="paragraph" style="text-align:left;"><strong>UseS mathematical concepts and constructs to express ideas and solve problems in computer science, and to communicate quantitative information.</strong><ul><li>Use mathematical ideas to solve problems in Computer Science and Information Technology.</li><li>Use mathematical thinking and models, charts, graphs, tables, figures, equations, and appropriate technologies to express ideas and concepts.</li><li>Demonstrate an understanding of discrete mathematical concepts used in computing: Articulate and apply the concepts of binary numbers, combinatorics, logic, truth tables, graph theory and algorithms to problems in Computer Science and Information Technology.</li><li>Analyze and evaluate the mathematical thinking and strategies of others.</li></ul><strong>Satisfied With:</strong><ul><li><strong>MATH 150, Calculus I</strong></li><li><strong>MATH 151, Calculus II</strong></li><li><strong>MATH 170, Discrete Mathematics&#8203;</strong></li></ul></div>
+<h2   >Core Outcome 4: Mathematics for Computing</h2>
 
-<h2 class="wsite-content-title" style="text-align:left;">Core Outcome 5: Interaction Design</h2>
+<div class="paragraph"><strong>UseS mathematical concepts and constructs to express ideas and solve problems in computer science, and to communicate quantitative information.</strong><ul><li>Use mathematical ideas to solve problems in Computer Science and Information Technology.</li><li>Use mathematical thinking and models, charts, graphs, tables, figures, equations, and appropriate technologies to express ideas and concepts.</li><li>Demonstrate an understanding of discrete mathematical concepts used in computing: Articulate and apply the concepts of binary numbers, combinatorics, logic, truth tables, graph theory and algorithms to problems in Computer Science and Information Technology.</li><li>Analyze and evaluate the mathematical thinking and strategies of others.</li></ul><strong>Satisfied With:</strong><ul><li><strong>MATH 150, Calculus I</strong></li><li><strong>MATH 151, Calculus II</strong></li><li><strong>MATH 170, Discrete Mathematics&#8203;</strong></li></ul></div>
 
-<div class="paragraph" style="text-align:left;"><strong>Demonstrate effective use of computer-based tools used in the design and creation of interactive applications and electronic media.</strong><ul><li>Design, create, and edit electronic images, sounds, videos, text, and motion.</li><li>Use multimedia authoring tools to design, manage, and execute an interactive multimedia project.</li></ul><strong>Satisfied With:</strong><ul><li><strong><a href="/cst-205-multimedia-programming.html">&#8203;&#8203;CST 205, Multimedia Design and Programming</a></strong></li></ul></div>
+<h2  >Core Outcome 5: Interaction Design</h2>
+
+<div class="paragraph"><strong>Demonstrate effective use of computer-based tools used in the design and creation of interactive applications and electronic media.</strong><ul><li>Design, create, and edit electronic images, sounds, videos, text, and motion.</li><li>Use multimedia authoring tools to design, manage, and execute an interactive multimedia project.</li></ul><strong>Satisfied With:</strong><ul><li><strong><a href="/cst-205-multimedia-programming.html">&#8203;&#8203;CST 205, Multimedia Design and Programming</a></strong></li></ul></div>
 </div>
 
   </div>
@@ -42,5 +40,55 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+h1 {
+  font-size: 50px;
+  padding: 100px;
+  color: #4c6b83;
+}
 
+h2 {
+  padding: 50px;
+  text-align: center;
+  color: #4c6b83;
+}
+
+.banner {
+  background: white;
+  border-style: solid;
+  border-radius: 10px;
+  border-width: 10px;
+  border-color: #4c6b83;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  color: #4c6b83;
+  display: inline-block;
+  margin: 0 10px;
+}
+
+p {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-weight: normal;
+  font-size: 20px;
+  line-height: 1.5;
+  color: #666666;
+}
+
+.csumb-content {
+  display: block;
+  font-weight: normal;
+  text-align: left;
+
+  margin: 20px 400px 20px 400px;
+}
+.paragraph{
+  position: relative;
+margin: 20px 400px 20px 400px;
+text-align: left;
+}
 </style>
